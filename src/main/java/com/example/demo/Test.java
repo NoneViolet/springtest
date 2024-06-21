5811aa8af5a9312ce2aea7a13186cd6ac2427815
noa <snoachi@yahoo.co.jp>
2024-06-21 12:55:40 +0900

    Initial Commit

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+
+public class Test {
+	private int score;
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+}
